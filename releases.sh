a24eca5f9e6ec953956764d7a4d19d032f5b2d06
Ben Overmyer <ben.overmyer@protolabs.com>
2019-05-10 13:27:35 -0500

    Add .exe to Windows builds

--- releases.sh
+++ releases.sh
@@ -2,4 +2,4 @@
 env GOOS=darwin GOARCH=amd64 go build -o build/barge-darwin-amd64 .
 env GOOS=linux GOARCH=386 go build -o build/barge-linux-386 .
 env GOOS=linux GOARCH=amd64 go build -o build/barge-linux-amd64 .
-env GOOS=windows GOARCH=amd64 go build -o build/barge-windows-amd64 .
+env GOOS=windows GOARCH=amd64 go build -o build/barge-windows-amd64.exe .
